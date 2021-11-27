d663c92721ac6dfa11252008d8c85b943ca949fd
Jacob Harris <jgharris314@gmail.com>
2021-11-27 00:26:13 -0700

    added About section. need to work on content probably

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,7 +2,27 @@ import React from "react";
 import { StyledAbout } from "./about.styles";
 import "../styles.css";
 const About = () => {
-	return <StyledAbout className="body-section">About</StyledAbout>;
+	return (
+		<StyledAbout className="body-section">
+			<div className="section">
+				Greetings! <br />
+				I'm Jacob Harris, a web developer located in Denver, Colorado.
+			</div>
+			<ul className="section">
+				<h4>Skills</h4>
+				<li className="section-item">React</li>
+				<li className="section-item">Express</li>
+				<li className="section-item">Node</li>
+				<li className="section-item">PostgreSQL</li>
+			</ul>
+			<ul className="section">
+				<h4>Hobbies</h4>
+				<li className="section-item">Kendama</li>
+				<li className="section-item">Learning</li>
+				<li className="section-item">Hiking</li>
+			</ul>
+		</StyledAbout>
+	);
 };
 
 export default About;
--- src/components/About/about.styles.js
+++ src/components/About/about.styles.js
@@ -1,3 +1,27 @@
 import styled from "styled-components";
 
-export const StyledAbout = styled.div``;
+export const StyledAbout = styled.div`
+	display: flex;
+	flex-direction: column;
+
+	align-items: center;
+	padding-top: 15px;
+	.section {
+		/* font-size: 2rem; */
+		background-color: rgba(255, 255, 255, 0.18);
+		width: 70%;
+		justify-content: center;
+		align-items: center;
+		padding: 0;
+		border-radius: 5px;
+		box-shadow: 0px 0px 5px white;
+		font-size: 2.4rem;
+		h4 {
+			font-size: 4.2rem;
+			margin: 0px 0px 15px 0px;
+		}
+		&-item {
+			list-style: none;
+		}
+	}
+`;
